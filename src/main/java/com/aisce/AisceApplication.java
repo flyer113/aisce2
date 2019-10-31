ea7ce0f850a1c2350bcddeed8d7fdbb986e8dca1
yangfan <flyer1010@163.com>
2019-10-31 15:24:31 +0800

    test

---
+++ src/main/java/com/aisce/AisceApplication.java
@@ -0,0 +1,13 @@
+package com.aisce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AisceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AisceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aisce/AisceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aisce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AisceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
